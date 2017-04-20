f60be26c7b6876b94dda80748d25f2b80c73c269
kandascan <boguslaw.boczkowski@gmail.com>
2017-04-20 15:54:26 +0200

    created project solution for verification sum control from xml file

---
+++ SumControlTests/VerificationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SumControlVeryfication;
+
+namespace SumControlTests
+{
+    [TestFixture]
+    public class VerificationTest
+    {
+        const string path = @"C:\Repository\verificationSumControl\file.xml";
+
+        [Test]
+        public void ShouldCalculateSumControl()
+        {
+            var result = SumControl.Process(path);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ SumControlVeryfication/SumControl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SumControlVeryfication
+{
+    public static class SumControl
+    {
+        private static int VerificationSumControl(int iteration, int confirmation, List<int> values)
+        {
+            if (confirmation <= 0)
+                return 0;
+
+            var sumControll = 1;
+
+            for (int i = 0; i < iteration; i++)
+            {
+                sumControll = values.Sum() * sumControll;
+            }
+
+            return 1;
+        }
+
+        public static int Process(string path)
+        {
+            var xmlFile = XElement.Load(path);
+
+            int iterations = Convert.ToInt16(xmlFile.Element("iterations").Value);
+            if (iterations < 1 || iterations > 8)
+            {
+                //throw new Exception("Iteration should be between <1, 8>");
+                return -1;
+            }
+
+            int confirmationdata = Convert.ToInt16(xmlFile.Element("confirmationdata").Value);
+
+            var values = new List<int>();
+
+            foreach (XElement value in xmlFile.Descendants("value"))
+            {
+                if (Convert.ToInt16(value.Value) < -10 || Convert.ToInt16(value.Value) > 10)
+                {
+                    //throw new Exception("Value should be a number between <-10, 10>");
+                    return -1;
+                }
+                    
+                values.Add(Convert.ToInt16(value.Value));
+            }
+
+            if (values.Count > 5 || values.Count == 0)
+            {
+                //throw new Exception("List of values cannot be grather than 5");
+                return -1;
+            }
+
+            return VerificationSumControl(iterations, confirmationdata, values);
+        }
+    }
+}
